58eae01e904cc1013ff4edb48985fdf1e2aeabf1
BaptisteMeynier <baptiste.meynier@gmail.com>
2019-04-17 23:15:37 +0200

    find how activate startup instance

--- portfolio-app/src/main/java/org/acme/quarkus/portfolio/app/provider/PortfolioInitialisation.java
+++ portfolio-app/src/main/java/org/acme/quarkus/portfolio/app/provider/PortfolioInitialisation.java
@@ -1,60 +1,71 @@
 package org.acme.quarkus.portfolio.app.provider;
 
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 import org.acme.quarkus.portfolio.business.repository.RepositoryPort;
 import org.acme.quarkus.portfolio.business.repository.spi.RepositoryProvider;
 import org.acme.quarkus.portfolio.business.service.PortfolioService;
 import org.acme.quarkus.portfolio.business.service.PortfolioServicePort;
-import org.acme.quarkus.portfolio.persistence.repository.SqlRepositoryAdapter;
+//import org.acme.quarkus.portfolio.persistence.repository.SqlRepositoryAdapter;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
+
+import org.jboss.logging.Logger;
+
 import java.nio.file.ProviderNotFoundException;
 import java.util.Iterator;
 import java.util.ServiceLoader;
-import org.jboss.logging.Logger;
 
 @ApplicationScoped
-@Singleton
-public class PortfolioProvider {
+public class PortfolioInitialisation {
 
-    private static final Logger LOG = Logger.getLogger(PortfolioProvider.class);
+    private static final Logger LOGGER = Logger.getLogger(PortfolioInitialisation.class);
     private static final String DEFAULT_PERSISTENCE_PROVIDER = "org.acme.quarkus.portfolio.persistence.repository.SqlRepositoryAdapter";
 
-    private RepositoryPort repositoryPort = defaultRepositoryProvider();
 
-    @Produces
-    @ApplicationScoped
-    public PortfolioServicePort getPortfolioService() {
-        return new PortfolioService(repositoryPort);
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("The application is starting...{}");
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application is stopping... {}");
     }
 
 
-    public static RepositoryPort defaultRepositoryProvider() {
+  //  private RepositoryPort repositoryPort = defaultRepositoryProvider();
+
+  /*  @Produces
+    public PortfolioServicePort getPortfolioService() {
+        return  new PortfolioService(null);
+       // return new PortfolioService(repositoryPort);
+    }
+*/
+
+  /*  public RepositoryPort defaultRepositoryProvider() {
         return provider(DEFAULT_PERSISTENCE_PROVIDER).create();
     }
 
-    public static RepositoryProvider provider(String providerName) {
-        LOG.info("1)" + providerName);
+    public RepositoryProvider provider(String providerName) {
+        LOGGER.info("1)" + providerName);
         ServiceLoader<RepositoryProvider> loader = ServiceLoader.load(RepositoryProvider.class);
-        LOG.info("2)" + loader);
+        LOGGER.info("2)" + loader);
         Iterator<RepositoryProvider> it = loader.iterator();
-        LOG.info("3)" + loader);
+        LOGGER.info("3)" + loader);
         while (it.hasNext()) {
-            LOG.info("4)");
+            LOGGER.info("4)");
             RepositoryProvider provider = it.next();
-            LOG.info("5)");
+            LOGGER.info("5)");
             if (providerName.equals(provider.getClass().getName())) {
-                LOG.info("6)");
+                LOGGER.info("6)");
                 return provider;
             }
-            LOG.info("7)");
+            LOGGER.info("7)");
         }
         throw new ProviderNotFoundException(String.format("Exchange Rate provider %s not found",providerName));
-    }
+    }*/
 
 }
